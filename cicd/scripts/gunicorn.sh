8ee0d177eb96666a1aef34afe7ccfac372a53d60
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 04:23:04 +0530

    WC1

--- cicd/scripts/gunicorn.sh
+++ cicd/scripts/gunicorn.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
-sudo cp /home/ec2-user/DjangoCICDPipeline/gunicorn/cicd/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ec2-user/DjangoCICDPipeline/gunicorn/cicd/gunicorn.service  /etc/systemd/system/gunicorn.service
+sudo cp /home/ubuntu/DjangoCICDPipeline/cicd/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/DjangoCICDPipeline/cicd/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
 sudo systemctl start gunicorn.service
 sudo systemctl enable gunicorn.service
-
-# DjangoCICDPipeline
--- cicd/scripts/start_app.sh
+++ cicd/scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.95.154.17"]/' /home/ec2-user/DjangoCICDPipeline/cicd/cicd/settings.py
+sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/DjangoCICDPipeline/cicd/blog/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
