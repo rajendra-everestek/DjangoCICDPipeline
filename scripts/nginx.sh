6ca9720781ee9e6007e1054547e4a9ba27ce08ff
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 05:47:49 +0530

    WC1

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/DjangoCICDPipeline/cicd/nginx/nginx.conf /etc/nginx/sites-available/cicd
+sudo cp /home/ubuntu/DjangoCICDPipeline/nginx/nginx.conf /etc/nginx/sites-available/cicd
 sudo ln -s /etc/nginx/sites-available/cicd /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/cicd /etc/nginx/sites-enabled
 #sudo nginx -t
